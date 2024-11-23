4a55a8633cc21e9b2bfdf126fdce50dd364fdef5
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-23 06:44:42 +0000

    views.py

--- rental_management/views.py
+++ rental_management/views.py
@@ -1,8 +1,52 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Building, Unit, Tenant, LeaseContract, Invoice
+from .serializers import BuildingSerializer, UnitSerializer, TenantSerializer, LeaseContractSerializer, InvoiceSerializer
 
-from .models import Building
+class BuildingViewSet(viewsets.ModelViewSet):
+    queryset = Building.objects.all()
+    serializer_class = BuildingSerializer
+    permission_classes = [IsAuthenticated]
 
+class UnitViewSet(viewsets.ModelViewSet):
+    queryset = Unit.objects.all()
+    serializer_class = UnitSerializer
+    permission_classes = [IsAuthenticated]
 
-def building_list(request):
-    buildings = Building.objects.all()
-    return render(request, 'building_list.html', {'buildings': buildings})
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        is_availaible = self.request.query_params.get('is_availaible')
+        if is_availaible is not None:
+            queryset = queryset.filter(is_availaible=is_availaible.lower() == 'true')
+            return queryset
+
+class TenantViewSet(viewsets.ModelViewSet):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+    permission_classes = [IsAuthenticated]
+
+class LeaseContractViewSet(viewsets.ModelViewSet):
+    queryset = LeaseContract.objects.all()
+    serializer_class = LeaseContractSerializer
+    permission_classes = [IsAuthenticated]
+
+        def get_queryset(self):
+            queryset = super().get_queryset()
+            is_active = self.request.query_params.get('is_active')
+            if is_active is not None:
+                queryset = queryset.filter(is_active=is_active.lower() == 'true')
+                return queryset
+
+class InvoiceViewSet(viewsets.ModelViewSet):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        is_paid = self.request.query_params.get('is_paid')
+        if is_paid is not None:
+            queryset = queryset.filter(is_paid=is_paid.lower() == 'true')
+            return queryset
