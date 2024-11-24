e28d8bf311f9fa7c5332a9e8097fce4bc2769e21
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-24 17:22:00 +0000

    Vies

--- rental_management/views.py
+++ rental_management/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import get_object_or_404, render
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
-
-from .models import Building, Invoice, LeaseContract, Tenant, Unit
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from .models import Building, Invoice, LeaseContract, Tenant, Unit, MaintenanceRequest, Expense, TenantBankAccount, RentReport
 from .serializers import (
     BuildingSerializer,
     InvoiceSerializer,
     LeaseContractSerializer,
     TenantSerializer,
     UnitSerializer,
+    MaintenanceRequestSerializer,
+    ExpenseSerializer,
+    TenantBankAccountSerializer,
+    RentReportSerializer
 )
 
 
@@ -18,21 +21,80 @@ class BuildingViewSet(viewsets.ModelViewSet):
     serializer_class = BuildingSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['name', 'address']
-    permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=['get'])
+    def units(self, request, pk=None):
+
+        building = get_object_or_404(Building, pk=pk)
+        units = building.unit_set.all()
+        serializer = UnitSerializer(units, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False, methods=['get'])
+    def statistics(self, request):
+        
+        total_buildings = Building.objects.count()
+        total_units = Unit.objects.count()
+        available_units = Unit.objects.filter(status='available')
+        return Response({
+            'total_buildings': total_buildings,
+            'total_units': total_units,
+            'available_units': available_units
+        })
 
 class UnitViewSet(viewsets.ModelViewSet):
     queryset = Unit.objects.all()
     serializer_class = UnitSerializer
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['unit_type', 'status', 'building']
-    permission_classes = [IsAuthenticated]
+    filterset_fields = ['building', 'unit_type', 'status']
+    @action(detail=False, methods=['get'])
+    def available_units(self, request):
+        available_units = Unit.objects.filter(status='available')
+        serializer = UnitSerializer(available_units, many=True)
+        return Response(serializer.data)
 
 class TenantViewSet(viewsets.ModelViewSet):
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['full_name', 'phone_number']
-    permission_classes = [IsAuthenticated]
+
+class MaintenceRequestViewSet(viewsets.ModelViewSet):
+    queryset = MaintenanceRequest.objects.all()
+    serializer_class = MaintenanceRequestSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['unit', 'is_resolved']
+    @action(detail=False, methods=['get'])
+    def unresolved(self, request):
+        unresolved_requests = MaintenanceRequest.objects.filter(is_resolved=False)
+        serializer = MaintenanceRequestSerializer(unresolved_requests, many=True)
+        return Response(serializer.data)
+
+class ExpenseViewSet(viewsets.ModelViewSet):
+    queryset = Expense.objects.all()
+    serializer_class = ExpenseSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['building', 'date']
+
+class TenantBankAccountViewSet(viewsets.ModelViewSet):
+    queryset = TenantBankAccount.objects.all()
+    serializer_class = TenantBankAccountSerializer
+
+class RentReportViewSet(viewsets.ModelViewSet):
+    queryset = RentReport.objects.all()
+    serializer_class = RentReportSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['building', 'generated_date']
+    @action(detail=False, methods=['get'])
+    def generated_report(self, request):
+        building_id = request.date.get('building')
+        total_income = request.date.get('total_income')
+        if not building_id or not total_income:
+            return Response({'error': 'يجب تقديم معلومات المبني و الدخل الاجمالي'}, status=status.HTTP_400_BAD_REQUEST)
+        building = get_object_or_404(Building, pk=building_id)
+        report = RentReport.objecst.create(building=building, total_income=total_income)
+        serializer = self.get_serializer(report)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class LeaseContractViewSet(viewsets.ModelViewSet):
     queryset = LeaseContract.objects.all()
