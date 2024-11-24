be9661bbe7754820bb9ad47790d92adaa933767f
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-24 18:47:40 +0000

    urls

--- rental_management/urls.py
+++ rental_management/urls.py
@@ -3,23 +3,28 @@ from rest_framework.routers import DefaultRouter
 
 from .views import (
     BuildingViewSet,
+    ExpenseViewSet,
+    MaintenceRequestViewSet,
+    RentReportViewSet,
+    TenantBankAccountViewSet,
     TenantViewSet,
     UnitViewSet,
-    building_detail,
-    building_list,
-    tenant_list,
-    unit_list,
 )
 
 router = DefaultRouter()
 router.register('buildings', BuildingViewSet, basename='building')
 router.register('units', UnitViewSet, basename='unit')
 router.register('tenants', TenantViewSet, basename='tenant')
+router.register('maintenance', MaintenceRequestViewSet, basename='maintenance')
+router.register('expenses', ExpenseViewSet, basename='expense')
+router.register('bank-accounts', TenantBankAccountViewSet, basename='bankaccount')
+router.register('rent-reports', RentReportViewSet, basename='rentreport')
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('buildings/', building_list, name='building_list'),
-    path('units/', unit_list, name='unit_list'),
-    path('tenants/', tenant_list, name='tenant_list'),
-    path('buildings/<int:pk>/', building_detail, name='building_detail'),
+    path('api/buildings/<int:pk>/units/', BuildingViewSet.as_view({'get': 'units'}), name='building-units'),
+    path('api/buildings/statistic/', BuildingViewSet.as_view({'get': 'statistics'}), name='building-statistics'),
+    path('api/units/available/', UnitViewSet.as_view({'get': 'available_units'}), name='available_units'),
+    path('api/maintenance/unresolved/', MaintenceRequestViewSet.as_view({'get': 'unresolved'}), name= 'unresolved-maintenance'),
+    path('api/rent-reports/generated/', RentReportViewSet.as_view({'post': 'generated_report'}), name='generated-rent-report'),
 ]
