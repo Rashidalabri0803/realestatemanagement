122b640f3118f02fa3574aa65b78289b711654e6
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-25 19:52:36 +0000

    rtl

--- rental_management/urls.py
+++ rental_management/urls.py
@@ -1,10 +1,26 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import BuildingViewSet
-from . import views
+
+from .views import (
+    BuildingViewSet,
+    ExpenseViewSet,
+    LeaseContractViewSet,
+    MaintenanceRequestViewSet,
+    NotifictionViewSet,
+    PaymentViewSet,
+    TenantViewSet,
+    UnitViewSet,
+)
 
 router = DefaultRouter()
 router.register('buildings', BuildingViewSet, basename='buildings')
+router.register('units', UnitViewSet, basename='units')
+router.register('tenants', TenantViewSet, basename='tenants')
+router.register('contracts', LeaseContractViewSet, basename='leasecontract')
+router.register('payments', PaymentViewSet, basename='payments')
+router.register('maintenance', MaintenanceRequestViewSet, basename='maintenance')
+router.register('expenses', ExpenseViewSet, basename='expenses')
+router.register('notifications', NotifictionViewSet, basename='notifications')
 
 urlpatterns = [
     path('api/', include(router.urls)),
