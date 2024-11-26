28822b67e33a613bfee0c89fce4be01235d9b28b
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-26 08:01:57 +0000

    urls

--- real_estate_management/urls.py
+++ real_estate_management/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework.permissions import AllowAny
+from django.contrib import admin
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -33,6 +34,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('api/v1/', include('rental_management.urls')),
     path('swagger/', schema_view.with_ui('swagger',cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',cache_timeout=0), name='schema-redoc'),
