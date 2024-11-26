9b3b667f593fcc3e6cdc40eb1fa19cbe442eb44a
Rashidalabri0803 <rashid92759346@gmail.com>
2024-11-26 11:28:30 +0000

    os

--- real_estate_management/settings.py
+++ real_estate_management/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'real_estate_management.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [os.path.join (BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
