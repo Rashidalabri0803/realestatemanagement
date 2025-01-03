6c7a25f00d22de3775375df95c457fcdd7b00af2
Rashidalabri0803 <rashid92759346@gmail.com>
2025-01-03 11:02:54 +0000

    u

--- properties/urls.py
+++ properties/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.property_list, name='property_list'),
+    path('create/', views.property_create, name='property_create'),
 ]
--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render, redirect
-
-from .models import Property
+from django.shortcuts import redirect, render
 
 from .forms import PropertyForm
+from .models import Property
+
 
 def property_list(request):
     properties = Property.objects.all()
     return render(request, 'properties/property_list.html', {'properties': properties})
+
+def property_create(request):
+    if request.method == 'POST':
+        form = PropertyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('property_list')
+    else:
+        form = PropertyForm()
+    return render(request, 'properties/property_create.html', {'form': form})
